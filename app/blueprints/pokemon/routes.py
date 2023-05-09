b8afa3ab0187b176090abd0b6e3005ef3eac5df7
Gian Carlo Torres <geanu@outlook.com>
2023-05-08 22:39:21 -0400

    Pokemon API complete

--- app/blueprints/pokemon/routes.py
+++ app/blueprints/pokemon/routes.py
@@ -1,10 +1,11 @@
 from flask import render_template 
 
-from . import bp 
+from . import bp, lp
 
 @bp.route('/pokemon')
 def pokemon():
     return render_template(
         'pokemon.jinja',
-        title="PokeFavorites: Pokemon"
+        title="PokeFavorites: Pokemon",
+        _dict=lp
     )
