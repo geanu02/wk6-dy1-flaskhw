94b9ea7ded46c550690c4b4cf4d7bb8459f52947
Gian Carlo Torres <geanu@outlook.com>
2023-05-08 23:18:58 -0400

    Finished homework

--- app/blueprints/pokemon/routes.py
+++ app/blueprints/pokemon/routes.py
@@ -1,11 +1,14 @@
-from flask import render_template 
+from flask import render_template
 
 from . import bp, lp
+from datetime import date
+
 
 @bp.route('/pokemon')
 def pokemon():
     return render_template(
         'pokemon.jinja',
         title="PokeFavorites: Pokemon",
+        _date=date.today(),
         _dict=lp
     )
