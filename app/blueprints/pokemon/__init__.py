377f91888143ec9144ba5e3ae7ef1efca2505fb7
Gian Carlo Torres <geanu@outlook.com>
2023-05-09 11:39:06 -0400

    Requirements Frozen

--- app/blueprints/pokemon/__init__.py
+++ app/blueprints/pokemon/__init__.py
@@ -5,16 +5,18 @@ import pokebase as pb
 
 bp = Blueprint('pokemon', __name__, url_prefix='/pokemon')
 
+
 def load_pokemon():
     _dict = {}
     rand_list = random.sample(range(1, 890), 6)
     for i, _n in enumerate(rand_list):
         p_num = _n
-        p_name = pb.pokemon(_n)
+        p_info = pb.pokemon(_n)
         p_sprite = pb.SpriteResource(
             'pokemon', _n, other=True, official_artwork=True)
-        _dict[i] = [p_num, p_name.name.title(), p_sprite.url]
+        _dict[i] = [p_num, p_info.name.title(), p_sprite.url]
     return _dict
+
 
 lp = load_pokemon()
 
